bfef347d9a467aebce840b8ec27fa495070d385b
Shamim Reja <shamimad23@gmail.com>
2025-07-02 13:58:41 +0530

    done

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,33 @@
+import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://758e7faa12836333e0832fa73e28292f@o4509543913684992.ingest.de.sentry.io/4509543917158480",
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  integrations:[
+    Sentry.browserTracingIntegration(),
+    //Sentry.metrics.metricsAggregatorIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+
+});
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
